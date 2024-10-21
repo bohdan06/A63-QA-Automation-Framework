40e5260114f2785d6da782e8608c2d8351ef9c81
bohdan06 <bohdan.ivasiv@testpro.io>
2024-10-21 11:56:25 -0400

    Homework-23
    Selected previous homework 19 and implemented Page Factory by using the initElements() method of Page Factory class.

--- src/test/java/pagefactory/HomePage.java
+++ src/test/java/pagefactory/HomePage.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 public class HomePage extends BasePage {
-    @FindBy (xpath = "//section[@id = 'playlists'] //a[contains(text(), 'Playlist23')]")
+    @FindBy (xpath = "//section[@id = 'playlists'] //a[contains(text(), 'Playlist23')][1]")
     WebElement playlistName;
     @FindBy(css = "div.success.show")
     WebElement deleteMsg;
